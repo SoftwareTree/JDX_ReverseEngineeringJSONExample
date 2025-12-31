3e1e216cee4272fb95d69e17e7b92953645d0127
Damodar Periwal <dperiwal@softwaretree.com>
2025-12-30 20:34:29 -0800

    Update compile scripts

--- compile.bat
+++ compile.bat
@@ -1,2 +1,12 @@
+# Find all .java files recursively and create sources.txt
 dir /s /B *.java > sources.txt
+
+REM Set JX_HOME to the root directory of JDX or Gilhari installation.
+REM Compile java source files with JDK 1.8 compatibility
+
+REM With JDK 1.8
 javac -d .\bin -cp .;%JX_HOME%\libs\jxclasses.jar;%JX_HOME%\external_libs\json-20240303.jar @sources.txt
+
+REM With JDK 1.9 or higher 
+REM javac -d .\bin --release 8 -cp .;%JX_HOME%\libs\jxclasses.jar;%JX_HOME%\external_libs\json-20240303.jar @sources.txt
+
